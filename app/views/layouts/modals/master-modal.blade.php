c44d7dffdcb5fa773adf992a35f15ffa871026d3
djdavies <jdavies.thfc@gmail.com>
2014-10-29 10:26:22 +0000

    fixed comment

--- app/views/layouts/modals/master-modal.blade.php
+++ app/views/layouts/modals/master-modal.blade.php
@@ -1,6 +1,6 @@
 <!-- Button trigger modal -->
 
-// The Main modal 
+<!-- The Main modal  -->
 <!-- Modal -->
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-dialog">
