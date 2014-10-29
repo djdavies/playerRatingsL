858f4524ba360641ed4ab0ae577f80ecea215244
Adam Jama <adamjama7@hotmail.co.uk>
2014-10-29 15:32:38 +0000

    fixed the validation form

--- app/views/welcome.blade.php
+++ app/views/welcome.blade.php
@@ -47,4 +47,5 @@
 <script src="jquery/jquery-1.11.1.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="js/bootstrap.min.js"></script>
+<script src="js/app.js"></script>
 </html>
