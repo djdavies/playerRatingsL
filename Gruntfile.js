fc73fe84f7213c31390954c8790f1faf9a0e6dc3
djdavies <jdavies.thfc@gmail.com>
2014-11-03 10:08:01 +0000

    fixed <<<HEAD rubbish in routes.php

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function (grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+	  uglify: {
+	    my_target: {
+	      files: {
+	        'public/js/output.min.js': ['public/js/bootstrap.js']
+	      }
+	    }
+	  }
+	});
+
+// Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+}
